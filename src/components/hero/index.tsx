611fa232e4ce3dd4cd927bf7bdb4bbabd9463159
Kacper Horbacz <deekappy@gmail.com>
2024-07-03 02:11:23 +0200

    Add files via upload

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,7 +6,9 @@ import line from '/public/assets/line.png'
 import PocketBase from 'pocketbase';
 
 import getData, { IData, IItem } from '../../data/data';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 (async () => {
@@ -16,11 +18,12 @@ const pb = new PocketBase('http://127.0.0.1:8090');
     } catch(e){}    
 })();
 
+
+let last = Date.now();
 const Hero = () => {
 
     
-
-    const [data, setData] = useState<IData>(getData);
+    
 
     const [soup, setSoup] = useState<any[]>([]);
     const [fish, setFish] = useState<any[]>([]);
@@ -31,9 +34,42 @@ const Hero = () => {
     const [warm_drinks, setWarmDrinks] = useState<any[]>([]);
     const [beer, setBeer] = useState<any[]>([]);
     
+    const load = () => {
+        const seconds = new Date().getSeconds();
+        if(seconds > 15 && seconds < 30 || seconds > 45 && seconds < 60){
+            setDrinks(true);
+        }else {
+            setDrinks(false);
+        }
 
-    setInterval(() => {
+        // allow every 5 seconds
+        if(Date.now() - last < 5_000){
+            console.log("skipped");
+            return;
+        }
 
+        // toast with custom css
+        toast(<>
+            <div className={style.notify}>
+                <div>
+                    <h3>Smakowało?</h3>
+                    <h5>Podziel się opinią na Google Maps</h5>
+                </div>
+                <div>
+                    <Image src="/assets/frame.png" width={180} height={180} alt="google" />
+                </div>
+            </div>
+        </>, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            className: style.toast,
+            bodyClassName: style.toast__content
+        });
+
+        last = Date.now();
+        console.log("loading");        
+    
         try {
             pb.collection("menu")?.getFullList(999).then(res => {
             let tempSoup: any[] = [];
@@ -103,12 +139,58 @@ const Hero = () => {
                 .filter(item => item.status)
                 .sort((a, b) => a.sort - b.sort));
 
-
+            console.log("loaded");
         })
         } catch(e){
+            console.log(e);
         }
-    }, 15_000);
+    }
 
+    setTimeout(() => {
+        load();
+    }, 1_000);
+    let loading:any = null;
+
+
+    useEffect(() => {
+        // This code runs when the component mounts
+        console.log("mounted");
+        loading = setInterval(() => {
+            load();
+        }, 15_000);
+        
+
+        // add component istead of text
+        toast(<>
+            <div className={style.notify}>
+                <div>
+                    <h3>Smakowało?</h3>
+                    <h5>Podziel się opinią na Google Maps</h5>
+                </div>
+                <div>
+                    <Image src="/assets/frame.png" width={180} height={180} alt="google" />
+                </div>
+            </div>
+        </>, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            className: style.toast,
+            bodyClassName: style.toast__content
+        });
+    
+        return () => {
+            console.log("unmounted");
+          // This code runs when the component unmounts
+            clearInterval(loading);
+        };
+    }, []);
+  
+    const [drinks, setDrinks] = useState(true);
+
+
+
+   
 
     return (
         <div>
@@ -173,7 +255,7 @@ const Hero = () => {
                         <div className={style.hero_content_element_extras_split}>
                             <div>
                                 {/* half of data.extras */}
-                                {extras.slice(0, data.extras.length / 2).map((item, index) => (
+                                {extras.slice(0, extras.length / 2).map((item, index) => (
                                     <div className={style.hero_list_element} key={index}>
                                         <div className={style.hero_list_element_data}>
                                             <div className={style.hero_list_element_name_smaller}>
@@ -194,7 +276,7 @@ const Hero = () => {
 
                             <div>
                                 {/* half of data.extras */}
-                                {extras.slice(data.extras.length / 2).map((item, index) => (
+                                {extras.slice(extras.length / 2).map((item, index) => (
                                     <div className={style.hero_list_element} key={index}>
                                         <div className={style.hero_list_element_data}>
                                             <div className={style.hero_list_element_name_smaller}>
@@ -240,7 +322,8 @@ const Hero = () => {
                     </div> */}
                     <div className={style.hero_content_element_food}>
                         <h1 className={style.hero_title}>
-                            DANIA OBIADOWE
+                            DANIA OBIADOWE 
+                            {}
                             <Image className={style.hero_title_line} src={line} width={150} height={10} alt="line" />
                         </h1>
                         {main_course.map((item, index) => (
@@ -261,7 +344,11 @@ const Hero = () => {
                                     </div>
                                 ))}
                     </div>
-                    <div className={style.hero_content_element_drinks_cold}>
+                    {
+                    
+                    drinks 
+                    ? (<>
+                        <div className={style.hero_content_element_drinks_cold}>
                         {/* split in two cold and beer */}
                         
                         <div className={style.hero_content_element_drinks_cold_split}>
@@ -290,18 +377,54 @@ const Hero = () => {
                                 ))}
                             </div>
 
-                             <div>
+                        </div>
+
+                    </div>
+                    
+                    <div className={style.hero_content_element_drinks_warm}>
+                        <h1 className={style.hero_title}>
+                            KAWA I HERBATA
+                        </h1>
+                        <Image className={style.hero_title_line} src={line} width={150} height={10} alt="line" />
+                        {warm_drinks.map((item, index) => (
+                                    <div className={style.hero_list_element} key={index}>
+                                        <div className={style.hero_list_element_data}>
+                                            <div className={style.hero_list_element_name_smaller}>
+                                                {item.name}
+                                            </div>
+                                            <div className={style.hero_list_element_price_smaller}>
+                                                {item.price} 
+                                            </div>
+                                        </div>
+                                        {item?.description && ( 
+                                            <p className={style.hero_list_element_description}>
+                                                {item.description || ""}
+                                            </p>
+                                        )}
+                                    </div>
+                                ))}
+                    </div>
+                    </>) : (<>
+                        <div className={style.hero_content_element_drinks_cold}>
+                        {/* split in two cold and beer */}
+                        
+                        <div className={style.hero_content_element_drinks_cold_split}>
+                            <div></div>
+                            <div>
                                 <h1 className={style.hero_title}>
                                     PIWO I WINO
                                     <Image className={style.hero_title_line} src={line} width={150} height={10} alt="line" />
                                 </h1>
-                                {beer.map((item, index) => (
+                                {beer.
+                                    // first half of data.beer
+                                    slice(0, beer.length / 2)
+                                .map((item, index) => (
                                     <div className={style.hero_list_element} key={index}>
                                         <div className={style.hero_list_element_data}>
-                                            <div className={style.hero_list_element_name}>
+                                            <div className={style.hero_list_element_name_smaller}>
                                                 {item.name}
                                             </div>
-                                            <div className={style.hero_list_element_price}>
+                                            <div className={style.hero_list_element_price_smaller}>
                                                 {item.price} 
                                             </div>
                                         </div>
@@ -313,9 +436,39 @@ const Hero = () => {
                                     </div>
                                 ))}
                             </div>
+
                         </div>
 
                     </div>
+                    
+                    <div className={style.hero_content_element_drinks_warm}>
+                        <h1 className={style.hero_title}>
+                            {/* invisible symbol */}
+                            <br />
+                        </h1>
+                        {
+                        beer.
+                            // second half of data.beer
+                            slice(beer.length / 2)
+                        .map((item, index) => (
+                                    <div className={style.hero_list_element} key={index}>
+                                        <div className={style.hero_list_element_data}>
+                                            <div className={style.hero_list_element_name_smaller}>
+                                                {item.name}
+                                            </div>
+                                            <div className={style.hero_list_element_price_smaller}>
+                                                {item.price} 
+                                            </div>
+                                        </div>
+                                        {item?.description && ( 
+                                            <p className={style.hero_list_element_description}>
+                                                {item.description || ""}
+                                            </p>
+                                        )}
+                                    </div>
+                                ))}
+                    </div>
+                    </>)}
 
                     <div className={style.hero_content_element_title}>
                         <Image src="/assets/logo.png" width={500} height={500} alt="logo" />
@@ -343,31 +496,10 @@ const Hero = () => {
                                     </div>
                                 ))}
                     </div>
-                    <div className={style.hero_content_element_drinks_warm}>
-                        <h1 className={style.hero_title}>
-                            KAWA I HERBATA
-                        </h1>
-                        <Image className={style.hero_title_line} src={line} width={150} height={10} alt="line" />
-                        {warm_drinks.map((item, index) => (
-                                    <div className={style.hero_list_element} key={index}>
-                                        <div className={style.hero_list_element_data}>
-                                            <div className={style.hero_list_element_name_smaller}>
-                                                {item.name}
-                                            </div>
-                                            <div className={style.hero_list_element_price_smaller}>
-                                                {item.price} 
-                                            </div>
-                                        </div>
-                                        {item?.description && ( 
-                                            <p className={style.hero_list_element_description}>
-                                                {item.description || ""}
-                                            </p>
-                                        )}
-                                    </div>
-                                ))}
-                    </div>
 
                 </div>
+                <ToastContainer />
+
             </Wrapper>
         </div>
     )
